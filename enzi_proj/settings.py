4427a95b12fa52bad057e07cf6202552546dd6aa
benie254 <davinci.monalissa@gmail.com>
2022-11-29 07:28:56 +0300

    update db confs & redeploy

--- enzi_proj/settings.py
+++ enzi_proj/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATABASES = {
        'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'ENGINE': 'django.db.backends.sqlite3',
            'NAME': os.getenv('DB_NAME'),
            'USER': os.getenv('DB_USER'),
            'PASSWORD': os.getenv('DB_PASSWORD'),
