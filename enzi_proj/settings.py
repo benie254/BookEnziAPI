ba81096267d7d6a32bb028f931598781c50cb928
benie254 <davinci.monalissa@gmail.com>
2022-11-29 04:24:31 +0300

    update app

--- enzi_proj/settings.py
+++ enzi_proj/settings.py
@@ -191,7 +191,7 @@ USE_L10N = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # extra places for collectstatic to find static files 
 STATICFILES_DIRS = [
