e4cc6859408cbb661b86dcb289d5c2543d8061b6
benie254 <davinci.monalissa@gmail.com>
2022-11-29 07:21:45 +0300

    update db confs & redeploy

--- enzi_proj/settings.py
+++ enzi_proj/settings.py
@@ -42,7 +42,7 @@ DATABASES = {
            'USER': os.getenv('DB_USER'),
            'PASSWORD': os.getenv('DB_PASSWORD'),
            'HOST': os.getenv('DB_HOST'),
-           'PORT': '*',
+           'PORT': '',
        }
 }
 
